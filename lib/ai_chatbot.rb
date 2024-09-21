aff30228cfdd780ff5daad0ecb1b4c9cae17ed3f
Sanket Tikhande <tikhandesanket@users.noreply.github.com>
2024-09-21 20:03:08 +0530

    Update ai_chatbot.rb

--- lib/ai_chatbot.rb
+++ lib/ai_chatbot.rb
@@ -15,7 +15,7 @@ module AiChatbot
 
     # Method to train the model with a new question-answer pair
     def self.train_model(new_question, new_answer)
-      stdout, stderr, status = Open3.capture3("python3", "#{__dir__}/ml_model.py", "train", new_question, new_answer)
+      stdout, stderr, status = Open3.capture3("python3", "#{__dir__}/ml_model.py", "train_model", new_question, new_answer)
 
       if status.success?
         return stdout.strip
@@ -23,5 +23,49 @@ module AiChatbot
         raise "Error: #{stderr}"
       end
     end
+
+
+       # Method to train the model with a new question-answer pair
+    def self.update_answer(existing_question, new_answer)
+      stdout, stderr, status = Open3.capture3("python3", "#{__dir__}/ml_model.py", "update_answer", existing_question, new_answer)
+
+      if status.success?
+        return stdout.strip
+      else
+        raise "Error: #{stderr}"
+      end
+    end
+
+    def self.update_or_delete_question(existing_question, new_question=nil)
+         stdout, stderr, status = new_question == nil ? Open3.capture3("python3", "#{__dir__}/ml_model.py", "update_or_delete_question", existing_question,"None") : Open3.capture3("python3", "#{__dir__}/ml_model.py", "update_or_delete_question", existing_question, new_question)
+
+      if status.success?
+        return stdout.strip
+      else
+        raise "Error: #{stderr}"
+      end
+    end
+
+    def self.list_questions
+      stdout, stderr, status = Open3.capture3("python3", "#{__dir__}/ml_model.py", "list_questions")
+
+      if status.success?
+        return stdout.strip
+      else
+        raise "Error: #{stderr}"
+      end
+    end
+
+    def self.list_answers
+      stdout, stderr, status = Open3.capture3("python3", "#{__dir__}/ml_model.py", "list_answers")
+
+      if status.success?
+        return stdout.strip
+      else
+        raise "Error: #{stderr}"
+      end
+    end
+
+
   end
 end
